95239183e227617e63bd5bd6a20cc016e6d1d5b8
Jack Stubblefield <jStubblefield@lbisit.onmicrosoft.com>
2023-06-22 22:03:05 -0500

    added test components to entrypoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import '@cloudscape-design/global-styles/index.css';
 import Button from '@cloudscape-design/components/button';
+import FileUpload from "@cloudscape-design/components/file-upload";
+import FormField from "@cloudscape-design/components/form-field";
+
 
 export const App = () => (
 	<>
 		<h1>this is my budget app</h1>
-		<Button>Cloudscape button</Button>
+        <Button>Cloudscape button</Button>
+        
 	</>
 );
+
