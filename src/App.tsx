ecc11d451dfedb7c831b2e1328d39b0d12a6d53d
Jack Stubblefield <jack011601@gmail.com>
2023-07-24 18:44:00 -0500

    removed state for access token

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import '@cloudscape-design/global-styles/index.css';
 import Button from '@cloudscape-design/components/button';
-import FileUpload from "@cloudscape-design/components/file-upload";
-import FormField from "@cloudscape-design/components/form-field";
-import Link from './Link/Link';
-
+import FileUpload from '@cloudscape-design/components/file-upload';
+import FormField from '@cloudscape-design/components/form-field';
+import React from 'react';
 
 export const App = () => (
 	<>
 		<h1>this is my budget app</h1>
-
-		<Link plaidLink={''}></Link>
-		
-        
 	</>
 );
-
