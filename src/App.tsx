8e01edff162ef9fbebd130088b23c26af86035bc
Jack Stubblefield <jack011601@gmail.com>
2023-07-25 14:47:45 -0500

    created transactions component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import '@cloudscape-design/global-styles/index.css';
 import Button from '@cloudscape-design/components/button';
-import FileUpload from "@cloudscape-design/components/file-upload";
-import FormField from "@cloudscape-design/components/form-field";
+import FileUpload from '@cloudscape-design/components/file-upload';
+import FormField from '@cloudscape-design/components/form-field';
 import Link from './Link/Link';
 import React from 'react';
-
+import Transactions from './Transactions/Transactions';
 
 export const App = () => (
 	<>
 		<h1>this is my budget app</h1>
+		<Transactions />
 		{/* <Link/> */}
 	</>
 );
-
