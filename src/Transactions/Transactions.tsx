2fdb2785969246e40d2c345751e3ebbe566cc552
Jack Stubblefield <jack011601@gmail.com>
2023-07-25 14:51:25 -0500

    created basic request

--- src/Transactions/Transactions.tsx
+++ src/Transactions/Transactions.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import axios from 'axios';
-
+const SERVER = import.meta.env.VITE_SERVER;
 
 const Transactions: React.FC = () => {
+    React.useEffect(() => {
+        const getAccessToken = async () => {
+            let response = await axios.get(`${SERVER}/transactions/get`);
+            console.log('transactions yo:', response.data);
+        }
+    }, [])
     return (
         <>
+            <h1>Transactions Start Here</h1>
+            
         </>
     )
 }
