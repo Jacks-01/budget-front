1887f7672d0a4b03691ab5b9393de65455cfca63
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-12-14 13:13:28 -0600

    set up header with router links

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-'use client'
+"use client";
+import React from "react";
 import {
   Box,
   Flex,
@@ -15,65 +16,53 @@ import {
   Stack,
   useColorMode,
   Center,
-} from '@chakra-ui/react'
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
-
-interface Props {
-  children: React.ReactNode
-}
-
-const NavLink = (props: Props) => {
-  const { children } = props
-
-  return (
-    <Box
-      as="a"
-      px={2}
-      py={1}
-      rounded={'md'}
-      _hover={{
-        textDecoration: 'none',
-        bg: useColorModeValue('gray.200', 'gray.700'),
-      }}
-      href={'#'}>
-      {children}
-    </Box>
-  )
-}
+} from "@chakra-ui/react";
+import {MoonIcon, SunIcon} from "@chakra-ui/icons";
+import {Link} from "react-router-dom";
 
 export default function Nav() {
-  const { colorMode, toggleColorMode } = useColorMode()
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const {colorMode, toggleColorMode} = useColorMode();
+  const {isOpen, onOpen, onClose} = useDisclosure();
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <Box>Logo</Box>
 
-          <Flex alignItems={'center'}>
-            <Stack direction={'row'} spacing={7}>
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button>
+                <Link to="/">Home</Link>
+              </Button>
+              <Button>
+                <Link to="/transactions">Transactions</Link>
+              </Button>
+              <Button>
+                <Link to="/link">Link</Link>
+              </Button>
+
               <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
               </Button>
 
               <Menu>
                 <MenuButton
                   as={Button}
-                  rounded={'full'}
-                  variant={'link'}
-                  cursor={'pointer'}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
                   minW={0}>
                   <Avatar
-                    size={'sm'}
-                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    size={"sm"}
+                    src={"https://avatars.dicebear.com/api/male/username.svg"}
                   />
                 </MenuButton>
-                <MenuList alignItems={'center'}>
+                <MenuList alignItems={"center"}>
                   <br />
                   <Center>
                     <Avatar
-                      size={'2xl'}
-                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                      size={"2xl"}
+                      src={"https://avatars.dicebear.com/api/male/username.svg"}
                     />
                   </Center>
                   <br />
@@ -92,5 +81,5 @@ export default function Nav() {
         </Flex>
       </Box>
     </>
-  )
+  );
 }
