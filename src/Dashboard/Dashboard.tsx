06ab0802617542cf10d57433ce3a0a60dcb16629
Jack Stubblefield <jack011601@gmail.com>
2023-12-15 19:51:46 -0600

    basic pie chart working

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -2,17 +2,52 @@ import React, {useEffect} from "react";
 import PieChart from "./PieChart";
 
 const Dashboard: React.FC = () => {
+  const [pieData, setPieData] = React.useState<Array<object>>([{}]);
+
   React.useEffect(() => {
     const localTransactions: string | null = localStorage.getItem("Trans Data");
 
     if (localTransactions) {
       const parsedTransactions = JSON.parse(localTransactions);
-      setPieData(parsedTransactions);
+      const newPieData = parsedTransactions.map(element => {
+        const data = {
+          id: element.Category,
+          label: element.Category,
+          value: parseInt(element.Amount),
+        };
+        return data;
+      });
+      const summarizedPieData = summarizeData(newPieData);
+      setPieData(summarizedPieData);
       return;
     }
   }, []);
 
-  const [pieData, setPieData] = React.useState<Array<object>>([{}]);
+  const summarizeData = (originalData: Array<object>, threshold = 1): Array<object> => {
+    const summarizedData: object[] = [];
+
+    // Create a map to aggregate values by label
+    const dataMap = new Map();
+
+    originalData.forEach(entry => {
+      const {label, value} = entry;
+
+      if (dataMap.has(label)) {
+        dataMap.set(label, dataMap.get(label) + value);
+      } else {
+        dataMap.set(label, value);
+      }
+    });
+
+    // Filter out entries below the threshold
+    dataMap.forEach((value, label) => {
+      if (value >= threshold) {
+        summarizedData.push({id: label, label, value});
+      }
+    });
+
+    return summarizedData;
+  };
   return (
     <>
       <h1>Dashboard</h1>
--- src/Dashboard/PieChart.tsx
+++ src/Dashboard/PieChart.tsx
@@ -13,28 +13,22 @@ interface props {
 
 const PieChart: React.FC<props> = ({data}) => {
   return (
+    <div style={{ height: '400px' }}>
     <ResponsivePie
       data={data}
-      margin={{top: 40, right: 80, bottom: 80, left: 80}}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
       innerRadius={0.5}
       padAngle={0.7}
       cornerRadius={3}
-      activeOuterRadiusOffset={8}
+      colors={{ scheme: 'category10' }}
       borderWidth={1}
-      borderColor={{
-        from: "color",
-        modifiers: [["darker", 0.2]],
-      }}
-      arcLinkLabelsSkipAngle={10}
-      arcLinkLabelsTextColor="#333333"
-      arcLinkLabelsThickness={2}
-      arcLinkLabelsColor={{from: "color"}}
+      borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
       arcLabelsSkipAngle={10}
-      arcLabelsTextColor={{
-        from: "color",
-        modifiers: [["darker", 2]],
-      }}
+      arcLinkLabelsTextOffset={6}
+      arcLabelsTextColor="#333333"
+      animate={true}
     />
+  </div>
   );
 };
 export default PieChart;
