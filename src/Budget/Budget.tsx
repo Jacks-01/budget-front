f4e05077e6861f296ac117b98965b0b5b212c5d0
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2024-01-01 15:29:48 -0600

    removed tabs in favor of one universal budget

--- src/Budget/Budget.tsx
+++ src/Budget/Budget.tsx
@@ -1,24 +1,10 @@
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
 import * as React from "react";
 
 const Budget: React.FC = () => {
   return (
     <>
       <h1>Budgets Page</h1>
-      <Tabs variant="soft-rounded" colorScheme="green">
-        <TabList>
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+
     </>
   );
 };
