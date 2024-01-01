573096a922f1b7d30663536f3c5fd7e2030bbb00
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2024-01-01 16:13:22 -0600

    basic form created for adding items

--- src/Budget/Budget.tsx
+++ src/Budget/Budget.tsx
@@ -1,10 +1,17 @@
+import {Box, Heading} from "@chakra-ui/react";
 import * as React from "react";
+import BudgetForm from "./BudgetForm";
+
+//TODO: Add dynamic stat trackers at the top of the page for: total budgeted, remaining, spent so far.
 
 const Budget: React.FC = () => {
   return (
     <>
-      <h1>Budgets Page</h1>
+      <Heading m={5}>Add spending categories to your budget!</Heading>
 
+      <Box width={500} m={10}>
+        <BudgetForm />
+      </Box>
     </>
   );
 };
--- src/Budget/BudgetForm.tsx
+++ src/Budget/BudgetForm.tsx
@@ -1,16 +1,41 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
 import React from "react";
+import {Form} from "react-router-dom";
 
-interface BudgetFormProps {
-    
-}
- 
+interface BudgetFormProps {}
+
 const BudgetForm: React.FC<BudgetFormProps> = () => {
-    return ( 
-        <>
-            <Heading>Budget Form</Heading>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Heading as={"h2"} size={"lg"} mb={4}>
+        Budget Form
+      </Heading>
+
+      <Form method="post">
+        <FormControl>
+          <FormLabel>Category</FormLabel>
+          <Input type="text" name="category" />
+          <FormHelperText>ex. Groceries, Pet, Home Improvement</FormHelperText>
+        </FormControl>
+
+        <FormControl mt={4}>
+          <FormLabel>Amount</FormLabel>
+          <Input type="number" placeholder="$0.00" name="amount" />
+        </FormControl>
+
+        <Button type="submit" variant={"outline"} colorScheme="green" mt={8}>
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
 export default BudgetForm;
