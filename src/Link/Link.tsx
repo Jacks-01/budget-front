d39c438bf076ec0b1f5e96f9e5cdb2c93745cdb9
Jack Stubblefield <jack011601@gmail.com>
2024-01-28 19:00:32 -0600

    added auth0 basics

--- src/Link/Link.tsx
+++ src/Link/Link.tsx
@@ -1,48 +1,48 @@
-import React from "react";
-import axios from "axios";
-import {usePlaidLink, PlaidLinkOnSuccess} from "react-plaid-link";
-import {Button} from "@chakra-ui/button";
-import {RepeatIcon} from "@chakra-ui/icons";
+import React from "react"
+import axios from "axios"
+import {usePlaidLink, PlaidLinkOnSuccess} from "react-plaid-link"
+import {Button} from "@chakra-ui/button"
+import {RepeatIcon} from "@chakra-ui/icons"
 
-const SERVER = import.meta.env.VITE_SERVER;
+const SERVER = import.meta.env.VITE_SERVER
 
 const Link: React.FC = () => {
-  const [token, setToken] = React.useState<string | null>(null);
-  const [publicToken, setPublicToken] = React.useState<string | null>(null);
+  const [token, setToken] = React.useState<string | null>(null)
+  const [publicToken, setPublicToken] = React.useState<string | null>(null)
 
   React.useEffect(() => {
     const getLinkToken = async () => {
-      const response = await axios.get(`${SERVER}/create_link_token`);
-      console.log(response.data);
-      setToken(response.data.link_token);
-    };
-    getLinkToken();
-  }, []);
+      const response = await axios.get(`${SERVER}/create_link_token`)
+      console.log(response.data)
+      setToken(response.data.link_token)
+    }
+    getLinkToken()
+  }, [])
 
   const onSuccess = React.useCallback<PlaidLinkOnSuccess>(
     (publicToken, metadata) => {
-      setPublicToken(publicToken);
+      setPublicToken(publicToken)
       axios.post(`${SERVER}/token_exchange`, {
         public_token: publicToken,
-      });
+      })
       // send public_token to your server
       // https://plaid.com/docs/api/tokens/#token-exchange-flow
-      console.log(publicToken, metadata);
+      console.log(publicToken, metadata)
     },
     [],
-  );
+  )
 
   const retryPublicToken = async (publicToken: string | null) => {
-    console.log(`public token: ${publicToken}`);
+    console.log(`public token: ${publicToken}`)
     const response = await axios.post(`${SERVER}/token_exchange`, {
       public_token: publicToken,
-    });
-    console.log("yo", response);
-  };
+    })
+    console.log("yo", response)
+  }
   const {open, ready} = usePlaidLink({
     token,
     onSuccess,
-  });
+  })
 
   return (
     <>
@@ -55,7 +55,7 @@ const Link: React.FC = () => {
         <RepeatIcon />{" "}
       </Button>
     </>
-  );
-};
+  )
+}
 
-export default Link;
+export default Link
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
-import * as React from "react";
-import {ChakraProvider} from "@chakra-ui/react";
-import {extendTheme} from "@chakra-ui/react";
-import * as ReactDOM from "react-dom/client";
-import App from "./App";
+import * as React from "react"
+import {ChakraProvider} from "@chakra-ui/react"
+import {extendTheme} from "@chakra-ui/react"
+import * as ReactDOM from "react-dom/client"
+import App from "./App"
+import {Auth0Provider} from "@auth0/auth0-react"
+
+const DOMAIN: string = import.meta.env.VITE_AUTH0_DOMAIN
+const CLIENT_ID: string = import.meta.env.VITE_AUTH0_CLIENT_ID
 
 const colors = {
   brand: {
@@ -10,13 +14,20 @@ const colors = {
     800: "#153e75",
     700: "#2a69ac",
   },
-};
-const theme = extendTheme({colors});
+}
+const theme = extendTheme({colors})
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <Auth0Provider
+      domain={DOMAIN}
+      clientId={CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}>
+      <ChakraProvider theme={theme}>
         <App />
-    </ChakraProvider>
+      </ChakraProvider>
+    </Auth0Provider>
   </React.StrictMode>,
-);
+)
